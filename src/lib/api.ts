b2713c9c78f5faf55043dcc566a133d7f0c4c061
Gabriel Prates Bitencourt <devprates@gmail.com>
2025-02-06 17:00:20 -0400

    Teste Deploy

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 export const frontendApi = axios.create({
-    baseURL: "http://localhost:3000/api",  // mudar para uma variavel de ambiente
+    baseURL: `${process.env.NEXT_PUBLIC_FRONTEND_URL}/api`,  // mudar para uma variavel de ambiente
     headers: {
         "Content-Type": "application/json"
     }
 });
 
 export const backendApi = axios.create({
-    baseURL: "http://localhost:8080",   // mudar para uma variavel de ambiente
+    baseURL: `${process.env.NEXT_PUBLIC_BACKEND_URL}`,   // mudar para uma variavel de ambiente
     headers: {
         "Content-Type": "application/json"
     }
